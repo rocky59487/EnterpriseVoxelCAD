6f3a84c64ea0df54f3344a377c3c06356cb78ba9
rocky59487 <127295711+rocky59487@users.noreply.github.com>
2026-03-04 21:26:37 +0800

    Create register_types.hpp

---
+++ src/gdextension/register_types.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_enterprise_voxel_cad_module(godot::ModuleInitializationLevel p_level);
+void uninitialize_enterprise_voxel_cad_module(godot::ModuleInitializationLevel p_level);
